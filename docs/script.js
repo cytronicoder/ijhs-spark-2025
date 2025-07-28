a80873cc56ed06450674c1c6519621e0b1d555bb
Zeyu Yao <novodoodle@gmail.com>
2025-07-28 18:31:55 +0800

    Add documentation site

---
+++ docs/script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('nav a[href^="#"]').forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault();
+        const target = document.querySelector(link.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
